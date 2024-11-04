d9aec807d2bdca7a97c344199ded9420d4da7576
vivianassis <vivianassis11@gmail.com>
2024-11-04 13:25:47 -0300

    Atualização projeto

--- src/main/java/org/example/pages/BasePage.java
+++ src/main/java/org/example/pages/BasePage.java
@@ -1,16 +1,32 @@
 package org.example.pages;
 
 import org.example.utils.WebDriverService;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class BasePage {
 
     protected final WebDriverService webDriverService;
 
+    //Classe para centralizar configurações das pages, como incluir o padrão PageFactory em todas
     public BasePage(WebDriverService webDriverService) {
         this.webDriverService = webDriverService;
         PageFactory.initElements(webDriverService.webDriver(), this);
     }
 
+    //A classe base page serve também para todos os métodos específicos que formos usar nas pages
+    public void selectOptionByValue(String selectId, String value){
+        WebElement selectElement = webDriverService.webDriver().findElement(By.id(selectId));
+        Select select = new Select(selectElement);
+        select.selectByValue(value);
+    }
+
+    public void selectOptionByVisibleText(String selectId, String text){
+        WebElement selectElement = webDriverService.webDriver().findElement(By.id(selectId));
+        Select select = new Select(selectElement);
+        select.selectByVisibleText(text);
+    }
 }
--- src/test/java/org/example/CucumberTest.java
+++ src/test/java/org/example/CucumberTest.java
@@ -6,8 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class) //Anotação JUnit para identificar o framework de teste
 
-@CucumberOptions( //Anotação do framework para definir o funcionamento do Cucumber
-        plugin = {"pretty", "html:src/reports/sample/report.html"}
+@CucumberOptions( // Anotação do framework para definir o funcionamento do Cucumber
+        features = "src/test/resources/org/example/features", // Caminho para os arquivos de feature
+        glue = "org.example", // Pacote onde estão as definições de passo
+        plugin = {
+                "pretty", // Formatação de saída legível
+                "html:src/reports/sample/report.html", // Relatório em HTML
+                "json:src/reports/sample/report.json", // Relatório em JSON
+                "junit:src/reports/sample/report.xml" // Relatório em XML para integração contínua
+        },
+        monochrome = true// Configuração para saída de console mais legível
 )
 
 public class CucumberTest {}
